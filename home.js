0e1c8a15d132ac40bccdd2813abf04c42f5f67b7
Chaiyoma <111121109+Chaiyoma@users.noreply.github.com>
2025-08-19 00:08:20 +0100

    Updated code
    updates

---
+++ home.js
@@ -0,0 +1,5 @@
+//This is the Home Screen
+
+//THIS IS A TEST SUBJECT
+
+
---
+++ script.js
@@ -0,0 +1,6 @@
+let introduction = 'My name is Chioma. I am a mobile developer'
+let age = 30;
+
+let gender = 'female';
+
+let hobby = 'reading';
