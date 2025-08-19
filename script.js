58e65f82b2b6478571d40aacc54fb5f0d7b37e93
Chaiyoma <111121109+Chaiyoma@users.noreply.github.com>
2025-08-19 01:09:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let introduction = 'My name is Chioma. I am a mobile developer'
 let age = 30;
+let firstName = 'Chioma';
+const GENDER = 'female';
 
-let gender = 'female';
+let occupation = 'Mobile Developer';
 
 let hobby = 'reading';
+
+console.log('I love coding so much, it is fun!');
+console.log('I also enjoy teaching other people');
